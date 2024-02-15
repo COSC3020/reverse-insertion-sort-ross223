2f3c56c07a36f4a7c0ce9f7523c33480153a2f65
ross223 <144744308+ross223@users.noreply.github.com>
2024-02-15 15:23:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
 function insertionSortReverse(array) {
+    for(var i = array.length - 2; i >= 0; i--) {
+        var val = array[i];
+        for(var j = i + 1; j < array.length && array[j] < val; j++) {
+          array[j - 1]  = array[j];
+        }
+        array[j - 1] = val;
+    }
     return array;
 }
